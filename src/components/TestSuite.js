9bea2faab40d64cc428fbd00ffcd73dcb657ed09
mac <mac@macs-MacBook-Pro-84.local>
2024-02-01 19:14:01 -0800

    feat: edit modal

--- src/components/TestSuite.js
+++ src/components/TestSuite.js
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import { TestPlanItem } from './TestPlanItem';
+import { TestSuiteEditModal } from './TestSuiteEditModal';
 import './TestSuite.scss';
 
 export const TestSuite = ({ suite }) => {
     const [expanded, setExpanded] = useState(false);
+    const [modalOpened, setModalOpened] = useState(false);
     const { test_suite_name, test_plans } = suite;
 
     const onExpand = () => {
         setExpanded(!expanded);
+    }
+
+    const onEdit = (ev) => {
+        ev.stopPropagation();
+        setModalOpened(true);
+    }
+
+    const onSave = (suite) => {
+        console.log('Suite Updated:', suite);
     }
 
     return (
@@ -15,7 +26,7 @@ export const TestSuite = ({ suite }) => {
             <div className='suite-header' onClick={onExpand}>
                 <span className={`suite-title ${expanded ? 'suite-expanded' : ''}`}>{test_suite_name}</span>
                 <span className='suite-count'>{test_plans.length} tests</span>
-                <button className='suite-edit-btn'>Edit</button>
+                <button className='suite-edit-btn' onClick={onEdit}>Edit</button>
             </div>
             {expanded && (
                 <div className='suit-plan-list'>
@@ -24,6 +35,7 @@ export const TestSuite = ({ suite }) => {
                     ))}
                 </div>
             )}
+            {modalOpened && <TestSuiteEditModal suite={suite} onSave={onSave} onClose={() => setModalOpened(false)} />}
         </div>
     );
 }
