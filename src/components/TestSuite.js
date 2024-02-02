f824ed57e6e154cf4a64c4eb18b488b0ca2ea710
mac <mac@macs-MacBook-Pro-84.local>
2024-02-01 17:54:42 -0800

    feat: implement ui

---
+++ src/components/TestSuite.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { TestPlanItem } from './TestPlanItem';
+import './TestSuite.scss';
+
+export const TestSuite = ({ suite }) => {
+    const [expanded, setExpanded] = useState(false);
+    const { test_suite_name, test_plans } = suite;
+
+    const onExpand = () => {
+        setExpanded(!expanded);
+    }
+
+    return (
+        <div className='suite-wrapper'>
+            <div className='suite-header' onClick={onExpand}>
+                <span className='suite-title'>{test_suite_name}</span>
+                <span className='suite-count'>{test_plans.length} tests</span>
+                <button className='suite-edit-btn'>Edit</button>
+            </div>
+            {expanded && (
+                <div className='suit-plan-list'>
+                    {test_plans.map((plan) => (
+                        <TestPlanItem key={plan.test_name} plan={plan} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
---
+++ src/containers/TestList.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { TestSuite } from '../components/TestSuite';
+
+export const TestList = () => {
+  const [testSuites, setTestSuites] = useState([]);
+
+  useEffect(() => {
+    const loadTestSuites = async () => {
+      try {
+        const response = await fetch('http://localhost:3456/test_suites');
+        const responseJSON = await response.json();
+        setTestSuites(responseJSON);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    loadTestSuites();
+  }, []);
+
+  return (
+    <div className='container'>
+      {testSuites.map(suite => (
+        <TestSuite key={suite.id} suite={suite} />
+      ))}
+    </div>
+  );
+}
